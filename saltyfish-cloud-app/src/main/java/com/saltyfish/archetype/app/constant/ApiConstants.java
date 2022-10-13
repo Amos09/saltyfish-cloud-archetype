3ba5af4f58e9323b2b4b3cabf3ccdbab510c7522
Clover <koali_zhang@163.com>
2022-10-13 17:58:37 +0800

    初始化项目

---
+++ saltyfish-cloud-app/src/main/java/com/saltyfish/archetype/app/constant/ApiConstants.java
@@ -0,0 +1,22 @@
+package com.saltyfish.archetype.app.constant;
+
+
+import com.saltyfish.framework.common.constant.RpcConstants;
+
+/**
+ * API 相关的枚举
+ */
+public class ApiConstants {
+
+    /**
+     * 服务名
+     *
+     * 注意，需要保证和 spring.application.name 保持一致
+     */
+    public static final String NAME = "archetype-server";
+
+    public static final String PREFIX = RpcConstants.RPC_API_PREFIX +  "/archetype";
+
+    public static final String VERSION = "1.0.0";
+
+}
