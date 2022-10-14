8ebe35369008f943db3ea2a6d5434b27c70fead1
Clover <koali_zhang@163.com>
2022-10-14 14:57:46 +0800

    添加模板

---
+++ archetype/src/main/resources/archetype-resources/__rootArtifactId__-adapter/src/main/java/com/saltyfish/ServerStartApplication.java
@@ -0,0 +1,29 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${groupId};
+
+import ${groupId}.framework.feign.annotation.SaltyfishEnableFeignClients;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * @author: 番薯(Amos)
+ * @dateTime: 9:51/25:07:2022
+ * @version: v1.0
+ * @description: 服务启动入口
+ */
+@SpringBootApplication
+@SaltyfishEnableFeignClients
+public class ServerStartApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(ServerStartApplication.class);
+    
+    public static void main(String[] args) {
+        ConfigurableApplicationContext run = SpringApplication.run(ServerStartApplication.class, args);
+        logger.info( run.getEnvironment().getProperty("spring.application.name") + " 服务启动成功; 端口号: " + run.getEnvironment().getProperty("server.port"));
+    }
+}
