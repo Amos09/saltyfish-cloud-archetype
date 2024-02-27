0de2f712afc658b2c66898130a0877aa997162d9
zhangminhua <koali09@163.com>
2024-02-27 18:19:59 +0800

    调整脚手架

--- saltyfish-cloud-server/src/main/java/com/saltyfish/ServerStartApplication.java
+++ saltyfish-cloud-server/src/main/java/com/saltyfish/ServerStartApplication.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ConfigurableApplicationContext;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.context.ConfigurableApplicationContext;
  * @description: 服务启动入口
  */
 @SpringBootApplication
+@EnableFeignClients
 public class ServerStartApplication {
     
     private static final Logger logger = LoggerFactory.getLogger(ServerStartApplication.class);
